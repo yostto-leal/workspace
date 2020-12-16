50a9490e908b5f362c815856cd8a5363192b85a9
yostto-leal <yosttoleal@gmail.com>
2020-12-16 17:58:32 -0300

    comit final

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -26,12 +26,9 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<Data.MongoDB>();        
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
-            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
